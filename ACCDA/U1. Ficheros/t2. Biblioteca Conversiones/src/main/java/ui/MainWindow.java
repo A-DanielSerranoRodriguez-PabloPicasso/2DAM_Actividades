3b3f2e3814478ca54bc69e8c7982f592e8616c6c
Daniel Serrano Rodríguez <a_daniel.serrano.rodriguez@iespablopicasso.es>
2022-09-29 20:43:48 +0200

    Casi terminado

--- ACCDA/U1. Ficheros/t2. Biblioteca Conversiones/src/main/java/ui/MainWindow.java
+++ ACCDA/U1. Ficheros/t2. Biblioteca Conversiones/src/main/java/ui/MainWindow.java
@@ -1,14 +1,25 @@
 package ui;
 
+import java.awt.Component;
+import java.awt.Dimension;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.border.TitledBorder;
-import javax.swing.JPanel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class MainWindow {
 	private enum Type {
@@ -19,8 +30,31 @@ public class MainWindow {
 
 	private JFrame frame;
 	private JList<String> fileList;
-	JScrollPane scrollPane;
-	private JPanel panel;
+	JScrollPane listScroll;
+	private JPanel misc;
+	private JPanel files;
+	private JScrollPane documentScroll;
+	private JTextPane txpPreview;
+	private Component hsMisc;
+	private Box vbMisc;
+	private Component vgMiscVB;
+	private Component vsMiscVB;
+	private Box hbFiles2;
+	private Box hbFiles1;
+	private Component vs1Files;
+	private Component vs1FilesHB1;
+	private Component hs1FilesHB1;
+	private JButton btnLoad;
+	private Component hs2FilesHB1;
+	private JButton btnDelete;
+	private Component hs1FilesHB2;
+	private Component vs2Files;
+	private Component hsFilesHB1;
+	private Component hgFilesHB;
+	private Component hs2FilesHB2;
+	private Box hbFiles2_1;
+	private Component horizontalGlue;
+	private Component hgMisc;
 
 	/**
 	 * Create the application.
@@ -37,29 +71,140 @@ public class MainWindow {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setVisible(true);
-		frame.setBounds(100, 100, 693, 473);
+		frame.setBounds(100, 100, 930, 592);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.X_AXIS));
 
 		components();
+		behaviour();
 	}
 //	jtextpaint + scroll
 
 	private void components() {
 		populateFiles(Type.ALL);
+
+		files = new JPanel();
+		frame.getContentPane().add(files);
+		files.setLayout(new BoxLayout(files, BoxLayout.Y_AXIS));
+		files.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+		vs1Files = Box.createVerticalStrut(20);
+		files.add(vs1Files);
+
+		hbFiles1 = Box.createHorizontalBox();
+		files.add(hbFiles1);
+
+		hs1FilesHB1 = Box.createHorizontalStrut(20);
+		hs1FilesHB1.setMaximumSize(new Dimension(20, 0));
+		hbFiles1.add(hs1FilesHB1);
+
+		btnLoad = new JButton("Cargar");
+		hbFiles1.add(btnLoad);
+
+		hs2FilesHB1 = Box.createHorizontalStrut(20);
+		hs2FilesHB1.setMaximumSize(new Dimension(20, 0));
+		hbFiles1.add(hs2FilesHB1);
+
+		btnDelete = new JButton("Eliminar");
+		hbFiles1.add(btnDelete);
+		
+		hsFilesHB1 = Box.createHorizontalStrut(20);
+		hsFilesHB1.setMaximumSize(new Dimension(20, 0));
+		hbFiles1.add(hsFilesHB1);
+		
+		hgFilesHB = Box.createHorizontalGlue();
+		hbFiles1.add(hgFilesHB);
+
+		vs1FilesHB1 = Box.createVerticalStrut(20);
+		files.add(vs1FilesHB1);
+		
+		hbFiles2_1 = Box.createHorizontalBox();
+		files.add(hbFiles2_1);
+
+		hbFiles2 = Box.createHorizontalBox();
+		hbFiles2_1.add(hbFiles2);
+
+		hs1FilesHB2 = Box.createHorizontalStrut(20);
+		hbFiles2.add(hs1FilesHB2);
 		fileList = new JList(filesName);
 		fileList.setLayoutOrientation(JList.VERTICAL);
-//		frame.getContentPane().add(fileList);
+
+		listScroll = new JScrollPane();
+		listScroll.setMinimumSize(new Dimension(200, 200));
+		listScroll.setMaximumSize(new Dimension(1000, 1000));
+		hbFiles2.add(listScroll);
+		listScroll.setViewportBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		listScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		listScroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		listScroll.setViewportView(fileList);
 		
-		scrollPane = new JScrollPane();
-		scrollPane.setViewportBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setViewportView(fileList);
-		frame.getContentPane().add(scrollPane);
+		hs2FilesHB2 = Box.createHorizontalStrut(20);
+		hbFiles2.add(hs2FilesHB2);
 		
-		panel = new JPanel();
-		frame.getContentPane().add(panel);
+		horizontalGlue = Box.createHorizontalGlue();
+		hbFiles2_1.add(horizontalGlue);
+
+		vs2Files = Box.createVerticalStrut(20);
+		files.add(vs2Files);
+
+		misc = new JPanel();
+		frame.getContentPane().add(misc);
+		misc.setLayout(new BoxLayout(misc, BoxLayout.X_AXIS));
+
+		vbMisc = Box.createVerticalBox();
+		misc.add(vbMisc);
+
+		vsMiscVB = Box.createVerticalStrut(20);
+		vbMisc.add(vsMiscVB);
+
+		documentScroll = new JScrollPane();
+		vbMisc.add(documentScroll);
+
+		txpPreview = new JTextPane();
+		txpPreview.setEditable(false);
+		documentScroll.setViewportView(txpPreview);
+
+		vgMiscVB = Box.createVerticalGlue();
+		vbMisc.add(vgMiscVB);
+
+		hsMisc = Box.createHorizontalStrut(30);
+		misc.add(hsMisc);
+		
+		hgMisc = Box.createHorizontalGlue();
+		misc.add(hgMisc);
+	}
+
+	private void behaviour() {
+		fileList.addListSelectionListener(new ListSelectionListener() {
+			public void valueChanged(ListSelectionEvent e) {
+				String file = fileList.getSelectedValue(), folder = getFolder(file), linea = "No disponible";
+
+				if (folder.equals("CSV")) {
+					String path = "src/main/resources/" + folder + "/", texto = "";
+					BufferedReader br = null;
+					try {
+						br = new BufferedReader(new FileReader(path + file));
+
+						while ((linea = br.readLine()) != null) {
+							texto += linea + "\n";
+						}
+						txpPreview.setText(texto);
+
+						br.close();
+					} catch (FileNotFoundException e1) {
+						e1.printStackTrace();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+
+				} else {
+					txpPreview.setText(linea);
+				}
+
+				frame.repaint();
+				frame.revalidate();
+			}
+		});
 	}
 
 	private void populateFiles(Type tipo) {
@@ -77,8 +222,8 @@ public class MainWindow {
 			for (int i = 0; i < datTotal; i++)
 				filesName[i + csvTotal] = datList[i];
 
-		}
 			break;
+		}
 		case CSV: {
 			File csv = new File(csvPath);
 			String[] csvList = csv.list();
@@ -88,8 +233,8 @@ public class MainWindow {
 			for (int i = 0; i < csvTotal - 1; i++)
 				filesName[i] = csvList[i];
 
-		}
 			break;
+		}
 		case DAT: {
 			File dat = new File(datPath);
 			String[] datList = dat.list();
@@ -99,9 +244,27 @@ public class MainWindow {
 			for (int i = 0; i < datTotal; i++)
 				filesName[i] = datList[i];
 
-		}
 			break;
+		}
 		}
 	}
 
+	private String getFolder(String file) {
+		String extension = file.substring(file.lastIndexOf(".") + 1, file.length());
+		switch (extension) {
+		case "csv": {
+			return "CSV";
+		}
+		case "dat": {
+			return "DAT";
+		}
+		default: {
+			return null;
+		}
+		}
+	}
+
+	private void populatePreview() {
+
+	}
 }
