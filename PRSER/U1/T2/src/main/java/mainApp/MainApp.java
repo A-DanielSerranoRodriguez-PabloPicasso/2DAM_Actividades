4365537dbc7288b50b999b7043aea2be21d91ed8
Daniel Serrano Rodríguez <a_daniel.serrano.rodriguez@iespablopicasso.es>
2022-10-05 14:43:42 +0200

    Rehecho imsorter. Reimplementar quicksort.

--- PRSER/U1/T2/src/main/java/mainApp/MainApp.java
+++ PRSER/U1/T2/src/main/java/mainApp/MainApp.java
@@ -1,14 +1,35 @@
 package mainApp;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import models.Sorter;
 
 public class MainApp {
 
 	public static void main(String[] args) {
-		int[] testeo = { 1, 3, 8, 99, 4, 2 };
-		Sorter.sorter(testeo, 0);
-		for (int i = 0; i < testeo.length; i++) {
-			System.out.println(testeo[i]);
+		ArrayList<Integer> testeo = new ArrayList<>();
+		String linea;
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(new File("src/main/resources/datos.txt")));
+			while((linea = br.readLine()) != null && !linea.isBlank()) {
+				testeo.add(Integer.parseInt(linea));
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+
+		Sorter.sorter(testeo);
+		for (Integer num : testeo) {
+			System.out.println(num);
+		}
+
 	}
 }
--- PRSER/U1/T2/src/main/java/models/Sorter.java
+++ PRSER/U1/T2/src/main/java/models/Sorter.java
@@ -1,37 +1,62 @@
 package models;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Sorter {
-	public static int[] sorter(int[] numeros, int pasadas) {
-		int posPivote = 0 + pasadas, pivote = numeros[posPivote];
+	public static ArrayList<Integer> sorter(ArrayList<Integer> lista) {
+		int length = lista.size();
+		int pivote = length / 2;
+		ArrayList<Integer> mayores = new ArrayList<>(), menores = new ArrayList<>();
+		int[] ordList1, ordList2;
 
-		if (posPivote != numeros.length - 1) {
-//			numeros = sorter(numeros, ++pasadas);
-			ProcessBuilder pb = new ProcessBuilder("java", "models.Sorter",
-					"sorter(" + numeros + ", " + ++pasadas + ")");
-			Process p = null;
-			String linea;
-			try {
-				p = pb.start();
-				BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-				while ((linea = br.readLine()) != null)
-					System.out.println(linea);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		for (int i = 0; i < length; i++) {
+			if (lista.get(i) >= lista.get(pivote))
+				mayores.add(lista.get(i));
+			else
+				menores.add(lista.get(i));
 		}
 
-		for (int i = posPivote; i < numeros.length; i++) {
-			if (numeros[i] < pivote) {
-				numeros[posPivote] = numeros[i];
-				numeros[i] = pivote;
-				posPivote = i;
-			}
-		}
+		menores = sort(menores);
+		mayores = sort(mayores);
 
-		return numeros;
+		lista.addAll(menores);
+		lista.addAll(mayores);
+
+		return lista;
 	}
+
+	private static ArrayList<Integer> sort(ArrayList<Integer> lista) {
+		lista.sort((o1, o2) -> o1 - o2);
+		return lista;
+	}
+
+//	public static int[] sorter(int[] numeros, int pasadas) {
+//		int posPivote = 0 + pasadas, pivote = numeros[posPivote];
+//
+//		if (posPivote != numeros.length - 1) {
+////			numeros = sorter(numeros, ++pasadas);
+//			ProcessBuilder pb = new ProcessBuilder("java", "models.Sorter",
+//					"sorter(" + numeros + ", " + ++pasadas + ")");
+//			Process p = null;
+//			String linea;
+//			try {
+//				p = pb.start();
+//				BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+//				while ((linea = br.readLine()) != null)
+//					System.out.println(linea);
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//
+//		for (int i = posPivote; i < numeros.length; i++) {
+//			if (numeros[i] < pivote) {
+//				numeros[posPivote] = numeros[i];
+//				numeros[i] = pivote;
+//				posPivote = i;
+//			}
+//		}
+//
+//		return numeros;
+//	}
 }
