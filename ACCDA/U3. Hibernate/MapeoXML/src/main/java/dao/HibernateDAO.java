0565247a4755f3f292c9600e7ed54fd079ae9399
Daniel Serrano Rodríguez <a_daniel.serrano.rodriguez@iespablopicasso.es>
2022-12-02 10:16:48 +0100

    Actividad mapeo completada

---
+++ ACCDA/U3. Hibernate/MapeoXML/src/main/java/dao/HibernateDAO.java
@@ -0,0 +1,24 @@
+package dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateDAO {
+	SessionFactory sf;
+	
+	public HibernateDAO() {
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure().build();
+		sf = new MetadataSources(ssr).buildMetadata().buildSessionFactory();
+	}
+	
+	public Session getSession() {
+		return sf.openSession();
+	}
+
+	public void closeSessionFactory(){
+		sf.close();
+	}
+}
---
+++ ACCDA/U3. Hibernate/MapeoXML/src/main/java/mainApp/MainApp.java
@@ -0,0 +1,25 @@
+package mainApp;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import dao.HibernateDAO;
+import models.Products;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		HibernateDAO hDao = new HibernateDAO();
+		Session session = hDao.getSession();
+		Query<Products> query = session.createQuery("select p from Products p");
+		
+		List<Products> productos = query.list();
+		
+		for(Products prod : productos) {
+			System.out.println(prod.getProductName());
+		}
+	}
+
+}
